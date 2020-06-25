96a18e3deca7e8bf682e26c1701d25d87831d6f3
Balaji Vengatesh M <balajimurugesan.2016@gmail.com>
2020-06-25 21:09:03 +0530

    new tests and logic

--- src/main/java/com/sap/co2calculator/main/C02EmissionCalculator.java
+++ src/main/java/com/sap/co2calculator/main/C02EmissionCalculator.java
@@ -1,49 +1,25 @@
 package com.sap.co2calculator.main;
+
+import java.util.HashMap;
+
 import com.sap.transportFactory.TransportFactory;
 
 public class C02EmissionCalculator {
 
-	private String[] getcommandlineargs(String[] args) {
+	private HashMap<String, String> getcommandlineargs(String[] args) {
 		Integer length = args.length;
-		String[] inputs = new String[length];
+		HashMap<String, String> inputs = new HashMap<String, String>();
 		int i = 0;
 		while (i < length) {
 
 			if (args[i].contains("--transportation-method"))
-				if(args[i].contains("=")) {
-					String [] arr = args[i].split("=");
-					inputs[0] = arr[1];
-				}
-				else
-				{
-				inputs[0] = args[i + 1];
-				}
+				addinputs(inputs, args, i, "--transportation-method");
 			else if (args[i].contains("--distance"))
-				if(args[i].contains("=")) {
-					String [] arr = args[i].split("=");
-					inputs[1] = arr[1];
-				}
-				else {
-				inputs[1] = args[i + 1]; 
-				}
+				addinputs(inputs, args, i, "--distance");
 			else if (args[i].contains("--unit-of-distance"))
-				if(args[i].contains("=")) {
-					String [] arr = args[i].split("=");
-					inputs[1] = arr[1];
-				}
-				else
-				{
-				inputs[2] = args[i + 1];
-				}
+				addinputs(inputs, args, i, "--unit-of-distance");
 			else if (args[i].contains("--output"))
-				if(args[i].contains("=")) {
-					String [] arr = args[i].split("=");
-					inputs[1] = arr[1];
-				}
-				else
-				{
-				inputs[3] = args[i + 1];
-				}
+				addinputs(inputs, args, i, "--output");
 
 			++i;
 		}
@@ -51,41 +27,172 @@ public class C02EmissionCalculator {
 
 	}
 
-	private Double convert(Double amount) {
+	private void addinputs(HashMap<String, String> inputs, String[] args, Integer i, String desc) {
 
-		return amount / 1000.0;
+		if (args[i].contains("=")) {
+			String[] arr = args[i].split("=");
+			inputs.put(desc, arr[1]);
+		} else {
+			inputs.put(desc, args[i + 1]);
+		}
 	}
 
-	private Double calcuateco2(String[] inputs,TransportFactory tf) {
-    
-		Double co2e = tf.getTransport(inputs[0]).getCo2e();
-		String uod = inputs[2];
-		if (uod != null &&  uod.contentEquals("m") ) {
-			co2e = convert(co2e);
+	private Double calcuateco2(HashMap<String, String> inputs, TransportFactory tf) {
+
+		Double co2e = tf.getTransport(inputs.get("--transportation-method")).getCo2e();
+		String uod = inputs.get("--unit-of-distance");
+		if (uod != null && uod.contentEquals("m")) {
+			co2e = co2e / 1000;
+		}
+
+		Double distance = Double.parseDouble(inputs.get("--distance"));
+		if (uod == null || uod.contentEquals("km")) {
+			distance = distance / 1000;
+		}
+
+		return Math.round((distance * co2e) * 10.0) / 10.0;
+
+	}
+
+	private Double calcuateco2foroutput(HashMap<String, String> inputs, TransportFactory tf) {
+
+		Double co2e = tf.getTransport(inputs.get("--transportation-method")).getCo2e();
+		String output = inputs.get("--output");
+		String uod = inputs.get("--unit-of-distance");
+		Double distance = Double.parseDouble(inputs.get("--distance"));
+	
+		if (output.contentEquals("kg") && uod.contentEquals("m")) {
+			co2e = co2e / 1000;
+			distance = distance / 1000;
+
+		}
+		else if (output.contentEquals("g") && uod.contentEquals("m") ) {
+			co2e = co2e / 1000;		
+
+		}
+		else if (output.contentEquals("kg") && uod.contentEquals("km")) {
+			co2e = co2e / 1000;		
+			
 		}
 		
 		
-
-		Double distance = Double.parseDouble(inputs[1]);
-		if ( uod == null || uod.contentEquals("km") ) {
-			distance = distance/1000;
-		}
+	
 		
-		return  Math.round((distance * co2e)*10.0)/10.0;
+		return Math.round((distance * co2e) * 10.0) / 10.0;
 
+	}
+
+	//Concatenate unit when output unit is present and emission is double 
+	private String appendUnit(HashMap<String, String> inp, Double co2, String output) {
+
+		return co2 + output;
+
+	}
+	//Concatenate unit when output unit is present and emission is integer 
+	private String appendUnit(HashMap<String, String> inp, Integer co2, String output) {
+
+		return co2 + output;
+
+	}
+	//Concatenate unit when output unit is absent and emission is double 
+	private String appendUnit(HashMap<String, String> inp, Double co2) {
+
+		String uod = inp.get("--unit-of-distance");
+
+		if (uod == null || uod.equalsIgnoreCase("km")) {
+
+			return co2 + "kg";
+		} else {
+			return co2 + "g";
+		}
+
+	}
+	//Concatenate unit when output unit is absent and emission is integer 
+	private String appendUnit(HashMap<String, String> inp, Integer co2) {
+
+		String uod = inp.get("--unit-of-distance");
+
+		if (uod == null || uod.equalsIgnoreCase("km")) {
+
+			return co2 + "kg";
+		} else {
+			return co2 + "g";
+		}
+
+	}
+
+	public String processemission(String[] args) {
+		Double calculatedco2 = 0.0;
+		HashMap<String, String> inp = getcommandlineargs(args);
+		String op = inp.get("--output");
+		if (op == null) {
+			calculatedco2 = calcuateco2(inp, new TransportFactory());
+
+			String text = Double.toString(Math.abs(calculatedco2));
+			int integerPlaces = text.indexOf('.');
+			if (text.charAt(integerPlaces + 1) == '0') {
+
+				return appendUnit(inp, calculatedco2.intValue());
+
+			}
+
+			return appendUnit(inp, calculatedco2);
+		} else {
+			calculatedco2 = calcuateco2foroutput(inp, new TransportFactory());
+			String text = Double.toString(Math.abs(calculatedco2));
+			int integerPlaces = text.indexOf('.');
+			if (text.charAt(integerPlaces + 1) == '0') {
+
+				return appendUnit(inp, calculatedco2.intValue(), op);
+
+			}
+			return appendUnit(inp, calculatedco2, op);
+		}
+	
 	}
 	
-	public Double processemission(String[] args) {
+	public String processemission1(String[] args) {
+		Double calculatedco2 = 0.0;
+		HashMap<String, String> inp = getcommandlineargs(args);
+		String output = inp.get("--output");
+		String uod = inp.get("--unit-of-distance");
+		if(output == null && uod == null )
+		{
+			inp.put("--output","kg");
+			inp.put("--unit-of-distance","km");
 		
-		String [] inp = getcommandlineargs(args);
-		Double calculatedco2 = calcuateco2(inp,new TransportFactory());
-		return  calculatedco2;
-		
+		}
+		else if(output == null && uod.contentEquals("km"))
+		{
+			inp.put("--output","kg");
+				
+		}
+		else if(output == null && uod.contentEquals("m"))
+		{
+			inp.put("--output","g");
+					
+		}
+		else if(uod == null && output.contentEquals("kg")  || uod == null && output.contentEquals("g")) {
+			
+			inp.put("--unit-of-distance","km");
+		}
+    		
+			calculatedco2 = calcuateco2foroutput(inp, new TransportFactory());
+			String text = Double.toString(Math.abs(calculatedco2));
+			int integerPlaces = text.indexOf('.');
+			if (text.charAt(integerPlaces + 1) == '0') {
+
+				return appendUnit(inp, calculatedco2.intValue(), inp.get("--output"));
+
+			}
+			return appendUnit(inp, calculatedco2, inp.get("--output"));
+
+	
 	}
 
 	public static void main(String[] args) {
 		C02EmissionCalculator co2emissioncalc = new C02EmissionCalculator();
-		Double calculatedco2 = co2emissioncalc.processemission(args);
+		String calculatedco2 = co2emissioncalc.processemission(args);
 
 		System.out.println("Your trip caused" + " " + calculatedco2 + " " + "of CO2-equivalent.");
 
--- src/test/java/com/sap/transportTest/C02EmissionCalcTest.java
+++ src/test/java/com/sap/transportTest/C02EmissionCalcTest.java
@@ -1,19 +1,20 @@
 package com.sap.transportTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 import com.sap.co2calculator.main.C02EmissionCalculator;
+import com.sap.transportFactory.TransportFactory;
+
 import org.junit.jupiter.api.Test;
 
 
 public class C02EmissionCalcTest {
-	
-
 	@Test
 	public void mediumdieselcartest() {	
 		C02EmissionCalculator co2emissioncalc = new C02EmissionCalculator();
 		String command = "--transportation-method medium-diesel-car --distance 15 --unit-of-distance km";
 		String []args = command.split("\\s");
-		assertEquals("2.6kg", co2emissioncalc.processemission(args));		
+		assertEquals("2.6kg", co2emissioncalc.processemission1(args));		
 	}
 	
 	@Test
@@ -21,7 +22,7 @@ public class C02EmissionCalcTest {
 		C02EmissionCalculator co2emissioncalc = new C02EmissionCalculator();
 		String command = "--distance 1800.5 --transportation-method large-petrol-car";
 		String []args = command.split("\\s");
-		assertEquals("507.7kg", co2emissioncalc.processemission(args));
+		assertEquals("507.7kg", co2emissioncalc.processemission1(args));
 	}
 	
 	@Test
@@ -29,7 +30,7 @@ public class C02EmissionCalcTest {
 		C02EmissionCalculator co2emissioncalc = new C02EmissionCalculator();
 		String command = "--transportation-method train --distance 14500 --unit-of-distance m";
 		String []args = command.split("\\s");
-		assertEquals("87g", co2emissioncalc.processemission(args));
+		assertEquals("87g", co2emissioncalc.processemission1(args));
 	}
 	
 	@Test
@@ -37,7 +38,7 @@ public class C02EmissionCalcTest {
 		C02EmissionCalculator co2emissioncalc = new C02EmissionCalculator();
 		String command = "--transportation-method train --distance 14500 --unit-of-distance m --output kg";
 		String []args = command.split("\\s");
-		assertEquals("0.1kg", co2emissioncalc.processemission(args));
+		assertEquals("0.1kg", co2emissioncalc.processemission1(args));
 			
 	}
 	
@@ -46,10 +47,68 @@ public class C02EmissionCalcTest {
 		C02EmissionCalculator co2emissioncalc = new C02EmissionCalculator();
 		String command = "--unit-of-distance=km --distance 15 --transportation-method=medium-diesel-car";
 		String []args = command.split("\\s");
-		assertEquals("2.6kg", co2emissioncalc.processemission(args));
+		assertEquals("2.6kg", co2emissioncalc.processemission1(args));
 		
 		
 	}
+	
+	@Test
+	public void trainkmoutputgmtest() {
+		C02EmissionCalculator co2emissioncalc = new C02EmissionCalculator();
+		String command = "--transportation-method train --distance 14.5 --unit-of-distance km --output g";
+		String []args = command.split("\\s");
+		assertEquals("87g", co2emissioncalc.processemission1(args));
+			
+	}
+	
+	@Test
+	public void trainkmoutputdefgmtest() {
+		C02EmissionCalculator co2emissioncalc = new C02EmissionCalculator();
+		String command = "--transportation-method train --distance 14.5  --output g";
+		String []args = command.split("\\s");
+		assertEquals("87g", co2emissioncalc.processemission1(args));
+			
+	}
+	
+	@Test
+	public void trainkmoutputdefkgtest() {
+		C02EmissionCalculator co2emissioncalc = new C02EmissionCalculator();
+		String command = "--transportation-method train --distance 14.5  --output kg";
+		String []args = command.split("\\s");
+		assertEquals("0.1kg", co2emissioncalc.processemission1(args));
+			
+	}
+	
+	@Test
+	public void trainmeteroutputgramtest() {
+		C02EmissionCalculator co2emissioncalc = new C02EmissionCalculator();
+		String command = "--transportation-method train --distance 14500 --unit-of-distance m --output g";
+		String []args = command.split("\\s");
+		assertEquals("87g", co2emissioncalc.processemission1(args));
+			
+	}
+	
+	@Test
+	public void TestInstancesTransportFactory() {
+		TransportFactory tf = new TransportFactory();
+		
+		assertEquals(142.0,  tf.getTransport("small-diesel-car").getCo2e());
+		assertEquals(154.0,  tf.getTransport("small-petrol-car").getCo2e());
+		assertEquals(73.0,  tf.getTransport("small-plugin-hybrid-car").getCo2e());
+		assertEquals(50.0,  tf.getTransport("small-electric-car").getCo2e());
+		assertEquals(171.0,  tf.getTransport("medium-diesel-car").getCo2e());
+		assertEquals(192.0,  tf.getTransport("medium-petrol-car").getCo2e());
+		assertEquals(110.0,  tf.getTransport("medium-plugin-hybrid-car").getCo2e());
+		assertEquals(58.0,  tf.getTransport("medium-electric-car").getCo2e());
+		assertEquals(209.0,  tf.getTransport("large-diesel-car").getCo2e());
+		assertEquals(282.0,  tf.getTransport("large-petrol-car").getCo2e());
+		assertEquals(126.0,  tf.getTransport("large-plugin-hybrid-car").getCo2e());
+		assertEquals(73.0,  tf.getTransport("large-electric-car").getCo2e());
+		assertEquals(27.0,  tf.getTransport("bus").getCo2e());
+		assertEquals(6.0,  tf.getTransport("train").getCo2e());
+			
+	}
+	
 	
 
 }
